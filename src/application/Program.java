e099c52e4a527f9c8ba07a36ecd4dcc011c8aced
Matheus Tavares <mt937169@gmail.com>
2025-06-05 00:10:07 -0300

    update implementation

--- src/application/Program.java
+++ src/application/Program.java
@@ -31,10 +31,16 @@ public class Program {
 			System.out.println(obj);
 		}
 		
-		System.out.println("\n===> TEST 4: Seller findAll <===");
+		System.out.println("\n===> TEST 4: Seller insert <===");
 		Seller newSeller = new Seller(null, "Greg", "greg@gmail.com", new Date(), 4000.0d, department);
 		sellerDao.insert(newSeller);
 		System.out.println("Inserted new ID = " + newSeller.getId());
+		
+		System.out.println("\n===> TEST 5: Seller update <===");
+		seller = sellerDao.findById(1);
+		seller.setName("DeadKeymon449");
+		sellerDao.update(seller);
+		System.out.println("Update completed");
 	}
 
 }
